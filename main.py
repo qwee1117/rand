a5e371181a44358a2485c60c0ab52af552e56e39
qwee1117 <qwee_1117@qq.com>
2026-04-24 00:26:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,14 @@ total_student = ["唐健程", "陈瑞格", "陈首名", "赵佳硕", "范志超"
                  "李博奥", "姜宇轩", "孙佳琪", "寇栖源", "王嘉蔚", "谭奕萌", "周若雪", "林思琪",
                  "白梦涵", "魏宇佳", "王一涵", "唐静然",
                  "孟江楠", "李雪坤", "曹悦然", "曾祥钊", "杨迪", "张传业", "李政霖", "侯佳诚",
-                 "郭展赫"]
+                 "郭展赫", "高越", "赵轩", "陈尚言"]
 root = tk.Tk()
 root.title("小考批卷人员抽取")
 root.geometry("1400x400")
 root.resizable(True, True)
-extra_list = ["高越", "赵轩", "陈尚言"]
+
 goal_list = []
 output = tk.Label(root, text=f"批卷人员为: {goal_list}", font=("Aril", 30))
-rg = [0, 1, 2, 3]
-tts = 0
 resout = ""
 
 
@@ -27,16 +25,12 @@ def main():
     global goal_list
     global total_student
     global output
-    global rg
-    global tts
     global resout
-    for i in range(4):
+    for i in range(6):
         c = len(total_student) - 1
         a = random.randint(0, c)
         goal_list.append(total_student[a])
         total_student.remove(total_student[a])
-    if random.randint(0, 10) >= 3:
-        goal_list[random.choice(rg)] = random.choice(extra_list)
     for i in range(len(goal_list)):
         resout += str(goal_list[i])
         resout += " "
@@ -50,7 +44,8 @@ def main():
                      "陈紫西",
                      "李博奥", "姜宇轩", "孙佳琪", "寇栖源", "王嘉蔚", "谭奕萌", "周若雪", "林思琪",
                      "白梦涵", "魏宇佳", "王一涵", "唐静然",
-                     "孟江楠", "李雪坤", "曹悦然", "曾祥钊", "杨迪", "张传业", "李政霖", "侯佳诚"]
+                     "孟江楠", "李雪坤", "曹悦然", "曾祥钊", "杨迪", "张传业", "李政霖", "侯佳诚", "高越", "赵轩",
+                     "陈尚言"]
     resout = ""
     return goal_list
 
